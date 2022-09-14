74ba2f1e3f7c9f124dcdf214b88f12655971f2e0
Lauren <laurenmunoz25@marlborough.org>
2022-09-14 09:36:24 -0700

    Update _Hello_World_.ino

--- _Hello_World_/_Hello_World_.ino
+++ _Hello_World_/_Hello_World_.ino
@@ -9,7 +9,7 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("we need to finish the chem work");
+  lcd.print("CHEM HW");
 }
 
 void loop() {
